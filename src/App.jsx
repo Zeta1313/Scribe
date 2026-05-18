e2a2a18d55d83b179279dcc685bdbd3d46507afc
Garrison Konschak <garrison.l.konschak@gmail.com>
2026-05-18 13:38:27 -0400

    Downloaded react-router-dom and made the changes to include it.

--- src/App.jsx
+++ src/App.jsx
@@ -8,4 +8,18 @@ function App() {
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <div>
+      <nav className="app-nav">
+        <Link to="/">Journal</Link>
+        <Link to="/history">History</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Journal />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </div>
+  )
+}
